e5def0ddf6a05c97435b7bd8c181b4ba895e1795
Marin Cabac <marin.cabac@gmail.com>
2018-02-08 15:36:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+sz=array.size
+z = sz-1
+
+
+ case
+when sz == 2
+  array.join(" and ")
+when sz == 3
+  array[z] = 'and '+ array[z]
+  array.join(", ")
+when sz>3
+  array[z] = 'and '+ array[z]
+  array.join(", ")
+else
+  array.join("")
+
+end
 end
