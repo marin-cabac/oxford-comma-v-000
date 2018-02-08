a162a067c5f79b22f1bd7621f51febcdbdbca8b9
Marin Cabac <marin.cabac@gmail.com>
2018-02-08 15:26:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,14 @@
 
 def oxford_comma(array)
 z = array.size-1
-if array.size == 2
+
+ case array
+when array.size == 2
   array.join(" and ")
-elsif array.size == 3
+when array.size == 3
   array[z] = 'and '+ array[z]
   array.join(", ")
-elsif array.size>3
+when array.size>3
   array[z] = 'and '+ array[z]
   array.join(", ")
 else
