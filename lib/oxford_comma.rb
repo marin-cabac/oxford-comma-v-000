0715e5245aaa69aa320eb91cea52045c85fdcac5
Marin Cabac <marin.cabac@gmail.com>
2018-02-08 14:39:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
 if array.size == 2
   array.join(" and ")
 elsif array.size == 3
-  array.join(", ")<<' and'
-
+  array.join(", ")
+  array.last='and'+ array.last
+  
 else
 array.join(",")
 end
