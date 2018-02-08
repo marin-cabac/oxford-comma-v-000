6a7971cdad31d306619a1e9a343b961070fc9f96
Marin Cabac <marin.cabac@gmail.com>
2018-02-08 14:42:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,10 @@ def oxford_comma(array)
 if array.size == 2
   array.join(" and ")
 elsif array.size == 3
-  array.join(", ")
+  
   array[2]=' and '+ array[2]
-  array
+  array.join(", ")
+  
 else
 array.join(",")
 end
