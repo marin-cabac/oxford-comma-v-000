ffea9e9251a4805d9b09d9cd5c699fab6a85f0d4
Marin Cabac <marin.cabac@gmail.com>
2018-02-08 15:24:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ last = array.size-1
 if array.size == 2
   array.join(" and ")
 elsif array.size == 3
-  array[last] = 'and '+ array[array.size-1]
+  array[last] = 'and '+ array[last]
   array.join(", ")
 elsif array.size>3
-  array[array.size-1] = 'and '+ array[array.size-1]
+  array[last] = 'and '+ array[last]
   array.join(", ")
 else
   array.join("")
