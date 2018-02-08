9e3a2b78a4db402cb83efc0ba8203b71d8d4949d
Marin Cabac <marin.cabac@gmail.com>
2018-02-08 14:49:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ elsif array.size == 3
   array[2]='and '+ array[2]
   array.join(", ")
 elsif array.size>3
-  array[array.size-1]='and '+ array[array.size-1]
+  array[array.size-1] = 'and '+ array[array.size-1]
   array.join(", ")
 else
   array.join("")
